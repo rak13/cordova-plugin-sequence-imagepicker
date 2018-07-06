eb373d34280a4ccc0b0178b57bcbb756a1c02ef7
Rakib Ahsan <ahsan_m@worksap.co.jp>
2018-07-06 18:50:57 +0800

    Fix imports

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -37,7 +37,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
